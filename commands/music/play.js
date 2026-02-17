16a5ec32bd53006003a7bcacd5e4af99319e13cd
DealerDeKebab <77692137+DealerDeKebab@users.noreply.github.com>
2026-02-17 09:44:01 +0100

    Update play.js

--- commands/music/play.js
+++ commands/music/play.js
@@ -32,7 +32,7 @@ async function fetchTrackInfo(query) {
 }
 
 function createYtDlpStream(url) {
-  return spawn('yt-dlp', ['-f', 'bestaudio[ext=webm]/bestaudio/best', '--no-playlist', '-o', '-', '--quiet', '--cookies', '/home/ubuntu/GamingBot/cookies.txt', url]).stdout;
+  return spawn('yt-dlp', ['-f', 'bestaudio/best', '--no-playlist', '-o', '-', '--quiet', '--extractor-args', 'youtube:player_client=android', url]).stdout;
 }
 
 async function startPlaying(queue, channel) {
